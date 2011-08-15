09cf8c75880464c3d8d2e540d88dc5c123259256
Radu Voicilas <radu.voicilas@gmail.com>
2011-08-15 18:07:03 +0300

    Tuareg mode enabled

--- init.el
+++ init.el
@@ -14,18 +14,25 @@
 (add-path "emacs/vendor/python-mode")
 (add-path "emacs/vendor/pymacs")
 (add-path "emacs/vendor/auto-complete")
+(add-path "emacs/vendor/tuareg")
 )
 
-; load my theme
-(load-file (concat emacs-root "emacs/colors/color-theme-blackboard.el"))
-(require 'color-theme)
-(color-theme-blackboard)
+; OCaml setup
+(add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
+(autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+(autoload 'camldebug "camldebug" "Run the caml debugger" t)
 
 (require 'python)
 (require 'auto-complete)
 (require 'magit)
 
+
 (global-font-lock-mode t)
 
 ; This is not really safe
 (setq make-backup-files nil)
+
+; load my theme
+(load-file (concat emacs-root "emacs/colors/color-theme-blackboard.el"))
+(require 'color-theme)
+(color-theme-blackboard)
