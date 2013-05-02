f939d2201064541087a7123d1f6b84e273900ba6
Radu Voicilas <radu.voicilas@gmail.com>
2013-05-02 19:36:43 +0300

    Misc improvements for new machine

--- raduv/scala.el
+++ raduv/scala.el
@@ -1,9 +1,3 @@
-(require 'scala-mode-auto)
 (require 'ensime)
-
-(add-hook 'scala-mode-hook
-	  '(lambda ()
-	     (yas/minor-mode-on)
-	     ))
 
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
