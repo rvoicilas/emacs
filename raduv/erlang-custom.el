bc69f1d0332b435abc9ec6da96780cbbcbddd257
Radu Voicilas <radu.voicilas@gmail.com>
2013-11-30 12:34:46 +0200

    Up erlang version + misc changes

--- raduv/erlang-custom.el
+++ raduv/erlang-custom.el
@@ -1,12 +1,20 @@
 (add-to-list
  'load-path
- (car (file-expand-wildcards "/usr/local/stow/OTP_R15B03-1/lib/erlang/lib/tools-*/emacs")))
+ (car (file-expand-wildcards "/usr/local/stow/OTP_R16B02/lib/erlang/lib/tools-*/emacs")))
 
-(setq erlang-root-dir "/usr/local/stow/OTP_R15B03-1/lib/erlang")
-(setq exec-path (cons "/usr/local/stow/OTP_R15B03-1/lib/erlang/bin" exec-path))
+(setq erlang-root-dir "/usr/local/stow/OTP_R16B02/lib/erlang")
+(setq exec-path (cons "/usr/local/stow/OTP_R16B02/lib/erlang/bin" exec-path))
+
+(add-to-list 'auto-mode-alist '("\\.rel$"))
+(add-to-list 'auto-mode-alist '("\\.app$"))
+(add-to-list 'auto-mode-alist '("\\.appSrc$"))
+(add-to-list 'auto-mode-alist '("\\.app.src$"))
+(add-to-list 'auto-mode-alist '("\\.hrl$"))
+(add-to-list 'auto-mode-alist '("\\.erl$"))
+(add-to-list 'auto-mode-alist '("\\.yrl$"))
 
 (require 'erlang-start)
-(require 'flymake)
+(require 'erlang-flymake)
 
 (require 'distel)
 (distel-setup)
