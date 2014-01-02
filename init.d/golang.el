815f248831fe40275e3196b46e8ec844794ccd63
Radu Voicilas <radu.voicilas@gmail.com>
2014-01-03 01:00:26 +0200

    Autocomplete works in Go

--- init.d/golang.el
+++ init.d/golang.el
@@ -1,1 +1,7 @@
 (require 'go-mode)
+
+(require 'auto-complete-config)
+
+(add-to-list 'ac-modes 'go-mode)
+(add-to-list 'ac-dictionary-directories "~/emacs/vendor/auto-complete/dict")
+(ac-config-default)
