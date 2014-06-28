a5b0a1643fe5d5cf0b792f653e59799e00dbed27
Radu Voicilas <radu.voicilas@gmail.com>
2014-06-28 23:42:24 +0300

    rinari-mode global

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -1,14 +1,13 @@
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Berksfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile.lock$" . ruby-mode))
 
 (require 'rvm)
 (require 'rinari)
 (require 'rspec-mode)
-
-(rvm-use-default)
 
 (eval-after-load 'ruby-mode
   '(progn
@@ -17,6 +16,10 @@
      (require 'rcodetools)
      (define-key ruby-mode-map (kbd "C-c C-c") 'xmp)))
 
+(global-rinari-mode)
+
 (setq ruby-deep-arglist nil)
 (setq ruby-deep-indent-paren nil)
 (setq ruby-deep-indent-paren-style nil)
+
+
