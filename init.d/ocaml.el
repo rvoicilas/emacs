095fee45a9597fe1650c86c9df73af9696a2b7e3
Radu Voicilas <radu.voicilas@gmail.com>
2014-09-16 12:26:37 +0300

    OCaml merlin + ocp-indent setup

--- init.d/ocaml.el
+++ init.d/ocaml.el
@@ -1,3 +1,15 @@
+
+(setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
+(add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+
+(require 'ocp-indent)
+(require 'merlin)
+
 (add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
 (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
 (autoload 'camldebug "camldebug" "Run the caml debugger" t)
+
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+
+;; Enable Merlin auto-completion
+(setq merlin-use-auto-complete-mode t)
