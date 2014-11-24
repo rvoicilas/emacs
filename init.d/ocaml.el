43ca38c6baba6e80897cf358a5cf0ce8ca06a43d
Radu Voicilas <radu.voicilas@gmail.com>
2014-11-24 05:21:46 +0200

    Testing some merlin 2.0 changes

--- init.d/ocaml.el
+++ init.d/ocaml.el
@@ -2,14 +2,21 @@
 (setq opam-share (substring (shell-command-to-string "opam config var share 2> /dev/null") 0 -1))
 (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
 
+;; (setq opam-bin (substring (shell-command-to-string "opam config var bin 2> /dev/null") 0 -1))
+;; (setq merlin-command (concat opam-bin "/ocamlmerlin"))
+
 (require 'ocp-indent)
 (require 'merlin)
 
 (add-to-list 'auto-mode-alist '("\\.ml[iylp]?" . tuareg-mode))
+
 (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
 (autoload 'camldebug "camldebug" "Run the caml debugger" t)
+(autoload 'merlin-mode "merlin" "Merlin mode" t)
 
 (add-hook 'tuareg-mode-hook 'merlin-mode)
 
 ;; Enable Merlin auto-completion
-(setq merlin-use-auto-complete-mode t)
+(setq merlin-use-auto-complete-mode 'easy)
+
+(setq merlin-command 'opam)
