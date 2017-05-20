e10e54d6b8406f5bbde89cb4a72b78f15152d2b3
Radu Voicilas <rvoicilas@users.noreply.github.com>
2017-05-20 13:06:56 +0300

    wip

--- init.d/golang.el
+++ init.d/golang.el
@@ -1,7 +1,10 @@
 (require 'go-mode)
-
+(require 'go-autocomplete)
 (require 'auto-complete-config)
 
 (add-to-list 'ac-modes 'go-mode)
 (add-to-list 'ac-dictionary-directories "~/emacs/vendor/auto-complete/dict")
 (ac-config-default)
+
+(add-hook 'before-save-hook #'gofmt-before-save)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
